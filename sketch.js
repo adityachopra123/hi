e1847747e01f01ae570dc32761a7b548770c618b
adityachopra123 <78090324+adityachopra123@users.noreply.github.com>
2021-05-07 18:50:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+//Create variables here
+var dog, happyDog, database, foodS, foodStock,dogImg,happyDogimg;
+function preload()
+{
+	//load images here
+  dogImg=loadImage("dogImg.png");
+  happyDogimg=loadImage("dogImg1.png");
+}
+
+function setup() {
+	createCanvas(500,500);
+  dog=createSprite(250,250);
+dog.addImage(dogImg);
+happyDog.addImage(happyDogimg);
+}
+
+
+function draw() {  
+background(46, 139, 87)
+if(keyWentDown(UP_ARROW)){
+  writeStock(foodS);
+  dog.addImage(dogHappy);
+}
+  drawSprites();
+  //add styles here
+stroke();
+}
+function readStock(data){
+  foodS=data.val();
+}
+function writeStock(x){
+  if(x<=0){
+    x=0
+  }
+  else{x=x-1}
+  database.ref('/').update({
+    food:x
+  })
+}
+
+
+
